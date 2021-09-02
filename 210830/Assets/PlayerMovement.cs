2700b73fcef6badb29526343d2e2fdd70e7521bb
TonyAhn <ejrrb8@gmail.com>
2021-09-02 20:58:24 +0900

    Try to fix Rb unassigned issue + Cube suddenly not moving

--- 210830/Assets/PlayerMovement.cs
+++ 210830/Assets/PlayerMovement.cs
@@ -5,23 +5,26 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody Rb;
 
-    public float forwardForce = 100f;
+    public float forwardForce = 200f;
 
-    public float sidewaysForce = 300f;
+    public float sidewaysForce = 50f;
 
     // FixedUpdate is better for physics event
     void FixedUpdate()
     {
         // Add a forward force
+        //Rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        
         Rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
         if (Input.GetKey("d"))
         {
-            Rb.AddForce(sidewaysForce, 0, 0);
+            Rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
         }
         if (Input.GetKey("a"))
         {
-            Rb.AddForce(-sidewaysForce, 0, 0);
+            Rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
         }
     }
 }
